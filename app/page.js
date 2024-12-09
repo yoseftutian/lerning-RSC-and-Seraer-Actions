a0a4cdc758a4bf7aa2c765bc482f06ad3a51de42
yosef-tutian <yoseftutian@gmail.com>
2024-12-09 20:13:24 +0200

    Add ErrorBoundary component to handle runtime errors gracefully

--- app/page.js
+++ app/page.js
@@ -2,21 +2,25 @@ import fs from "node:fs/promises";
 
 import { Suspense } from "react";
 import UsePromiseDemo from "./components/UsePromisesDemo";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 export default async function Home() {
-  const fetchUsersPromise = new Promise((resolve) =>
+  const fetchUsersPromise = new Promise((resolve, reject) =>
     setTimeout(async () => {
       const data = await fs.readFile("dummy-db.json", "utf-8");
       const users = JSON.parse(data);
-      resolve(users);
+      // resolve(users);
+      reject(new Error("Error!"));
     }, 2000)
   );
 
   return (
     <main>
-      <Suspense fallback={<p>Loading users...</p>}>
-        <UsePromiseDemo usersPromise={fetchUsersPromise} />
-      </Suspense>
+      <ErrorBoundary fallback={<p>Somthing went wrong!</p>}>
+        <Suspense fallback={<p>Loading users...</p>}>
+          <UsePromiseDemo usersPromise={fetchUsersPromise} />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
