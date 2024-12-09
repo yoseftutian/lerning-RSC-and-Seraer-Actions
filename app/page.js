02215ec56907eaaefa1494e0666cb3d747eae8dd
yosef-tutian <yoseftutian@gmail.com>
2024-12-09 19:59:31 +0200

    Add RSC component with data fetching and counter functionality

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
-import ServerActionsDemo from "./components/ServerActionsDemo";
+import fs from "node:fs/promises";
 
-export default function Home() {
+import { Suspense } from "react";
+import UsePromiseDemo from "./components/UsePromisesDemo";
+
+export default async function Home() {
+  const fetchUsersPromise = new Promise((resolve) =>
+    setTimeout(async () => {
+      const data = await fs.readFile("dummy-db.json", "utf-8");
+      const users = JSON.parse(data);
+      resolve(users);
+    }, 2000)
+  );
+
   return (
     <main>
-      <ServerActionsDemo />
+      <Suspense fallback={<p>Loading users...</p>}>
+        <UsePromiseDemo usersPromise={fetchUsersPromise} />
+      </Suspense>
     </main>
   );
 }
